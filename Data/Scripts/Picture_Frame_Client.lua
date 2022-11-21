63648d6a5c9f3b544545b58418327e0e16be9d29
CommanderFoo <pixeldepth@gmail.com>
2022-11-21 16:58:49 +0000

    Update Picture_Frame_Client.lua

--- Data/Scripts/Picture_Frame_Client.lua
+++ Data/Scripts/Picture_Frame_Client.lua
@@ -1,117 +1,156 @@
-local INVENTORY_API = require(script:GetCustomProperty("InventoryAPI"))
+local API = require(script:GetCustomProperty("InventoryAPI"))
+local TWEEN = require(script:GetCustomProperty("Tween"))
+local QUEUE = require(script:GetCustomProperty("Queue"))
 
-local ROOT = script:GetCustomProperty("Root"):WaitForObject()
-local PICTURE_FRAMES = ROOT:GetCustomProperty("PictureFrames"):WaitForObject()
+local SLOTS = script:GetCustomProperty("Slots"):WaitForObject()
+local PLAYER_INVENTORY = script:GetCustomProperty("PlayerInventory"):WaitForObject()
 
-while(_G.tokens == nil) do
-	Task.Wait()
+local localPlayer = Game.GetLocalPlayer()
+local inventory = nil
+local tween = nil
+local is_open = false
+
+local fetchingTokens = false
+local tokenQueue = QUEUE:new() ---type Queue
+local elapsedTime = 0
+local canFetchTokens = false
+
+_G.tokens = {}
+
+local function on_change(c)
+	PLAYER_INVENTORY.x = c.x
 end
 
-local current_frame = nil
+local function on_complete()
+	tween = nil
+end
 
-local function clear_outline()
-	if(current_frame ~= nil) then
-		current_frame.parent:FindDescendantByName("Outline"):SetSmartProperty("Enabled", false)
+local function open_inventory()
+	API.EnableCursor()
+
+	tween = TWEEN:new(1.2, { x = PLAYER_INVENTORY.x }, { x = -50 }, TWEEN.Easings.Out_Elastic, on_change, on_complete)
+	is_open = true
+
+	Events.Broadcast("inventory.opened")
+end
+
+local function close_inventory()
+	API.ClearDraggedItem(true)
+	API.DisableCursor()
+
+	tween = TWEEN:new(.6, { x = PLAYER_INVENTORY.x }, { x = 450 }, TWEEN.Easings.In_Quint, on_change, on_complete)
+	is_open = false
+
+	Events.Broadcast("inventory.closed")
+end
+
+local function InventoryChanged(inv, slot)
+	local item = inv:GetItem(slot)
+	local childIcon = SLOTS:GetChildren()[slot]:FindChildByName("Icon")
+	local childCount = childIcon:FindChildByName("Count")
+
+	if item ~= nil then
+		local address = item:GetCustomProperty("ContractAddress")
+		local tokenId = item:GetCustomProperty("TokenId")
+
+		if(_G.tokens[address .. tokenId] == nil) then
+			tokenQueue:push({
+
+				address = address,
+				tokenId = tokenId,
+				childIcon = childIcon,
+				slot = slot
+
+			})
+		elseif(_G.tokens[address .. tokenId] ~= nil) then
+			childIcon:SetBlockchainToken(_G.tokens[address .. tokenId])
+			childIcon.visibility = Visibility.FORCE_ON
+		end
+	else
+		childIcon.visibility = Visibility.FORCE_OFF
 	end
 end
 
-local function show_outline()
-	if(current_frame ~= nil) then
-		current_frame.parent:FindDescendantByName("Outline"):SetSmartProperty("Enabled", true)
-	end
-end
+local function ConnectSlotEvents()
+	for index, slot in ipairs(SLOTS:GetChildren()) do
+		local button = slot:FindChildByName("Button")
+		local icon = slot:FindChildByName("Icon")
 
-function Tick()
-	local forward = Game.GetLocalPlayer():GetViewWorldRotation() * Vector3.FORWARD
-	local start = Game.GetLocalPlayer():GetViewWorldPosition()
-	local fin = start + forward * 2000
-	local hit = World.Boxcast(start, fin, Vector3.New(100, 100, 100), {
-		
-		ignorePlayers = true
-	
-	})
-
-	if(hit) then
-		--CoreDebug.DrawBox(hit:GetImpactPosition(), Vector3.New(100, 100, 100))
-
-		if(hit.other.name == "Frame") then
-			if(current_frame ~= nil and current_frame ~= hit.other) then
-				clear_outline()
-			else
-				current_frame = hit.other
-				show_outline()
-			end
-		elseif(current_frame ~= nil) then
-			clear_outline()
-			current_frame = nil
+		if(button ~= nil and icon ~= nil and button.isInteractable) then
+			button.pressedEvent:Connect(API.OnSlotPressedEvent, inventory, slot, index)
 		end
 	end
 end
 
 local function on_action_pressed(player, action)
-	if(action == "Shoot" and current_frame ~= nil) then
-		if(INVENTORY_API.ACTIVE.hasItem) then
-			local item = INVENTORY_API.ACTIVE.inventory:GetItem(INVENTORY_API.ACTIVE.slotIndex)
-			local address = item:GetCustomProperty("ContractAddress")
-			local tokenId = item:GetCustomProperty("TokenId")
-
-			current_frame.parent:FindDescendantByType("UIContainer").visibility = Visibility.INHERIT
-	
-			if(_G.tokens ~= nil and _G.tokens[address .. tokenId]) then
-				current_frame.parent:FindDescendantByName("Picture"):SetBlockchainToken(_G.tokens[address .. tokenId])
-				current_frame.parent:FindDescendantByName("Info").text = _G.tokens[address .. tokenId].name
-				Events.BroadcastToServer("frame.set", address, tokenId, current_frame:GetReference(), player.name)
-			end
-
-			INVENTORY_API.RemoveItemSlotPressed()
+	if(action == "Open / Close Inventory") then
+		if(is_open) then
+			close_inventory()
 		else
-			Events.Broadcast("inventory.open")
+			open_inventory()
 		end
 	end
 end
 
-local function update_frame(key, entry)
-	for index, frame in ipairs(PICTURE_FRAMES:GetChildren()) do
-		if(string.find(frame.id, key)) then
-			Task.Spawn(function()
-				pcall(function()
-					local token = Blockchain.GetToken(entry[2], entry[3])
+local function LoadToken()
+	local current = tokenQueue:pop()
 
-					if(token) then
-						_G.tokens[entry[2] .. entry[3]] = token
-						frame:FindDescendantByType("UIContainer").visibility = Visibility.INHERIT
-						frame:FindDescendantByName("Picture"):SetBlockchainToken(token)
-						frame:FindDescendantByName("Info").text = token.name
-					end
-				end)
-			end)
+	fetchingTokens = true
 
-			Task.Wait(8)
-		end
-	end
-end
+	if(current ~= nil) then
+		local status, err = pcall(function()
+			local token = Blockchain.GetToken(current.address, current.tokenId)
 
-local function update_frames(player, key)
-	if(key == "frames") then
-		local data = player:GetPrivateNetworkedData(key)
+			if(token) then
+				_G.tokens[current.address .. current.tokenId] = token
 
-		if(data ~= nil) then
-			for key, entry in pairs(data) do
-				update_frame(key, entry)
+				current.childIcon:SetBlockchainToken(token)
+				current.childIcon.visibility = Visibility.FORCE_ON
+
+				print(string.format("Success: Token %s for slot %s. Time took %s", current.tokenId, current.slot, elapsedTime))
 			end
+		end)
+
+		if(not status) then
+			local _, e = CoreString.Split(err, "failed:")
+
+			print(string.format("Fail: Token %s for slot %s. Time took %s. Error: %s", current.tokenId, current.slot, elapsedTime, e))
 		end
+	end
+
+	elapsedTime = 0
+	Task.Wait(4)
+	fetchingTokens = false
+end
+
+while inventory == nil do
+	inventory = localPlayer:GetInventories()[1]
+	Task.Wait()
+end
+
+for i, item in pairs(inventory:GetItems()) do
+	InventoryChanged(inventory, i)
+end
+
+function Tick(dt)
+	if(tween ~= nil) then
+		tween:tween(dt)
+	end
+
+	elapsedTime = elapsedTime + dt
+
+	if(canFetchTokens and not fetchingTokens and not tokenQueue:is_empty()) then
+		Task.Spawn(LoadToken)
 	end
 end
 
+inventory.changedEvent:Connect(InventoryChanged)
 Input.actionPressedEvent:Connect(on_action_pressed)
 
-Events.Connect("frame.set", function(address, tokenId, frame)
-	if(_G.tokens ~= nil and _G.tokens[address .. tokenId]) then
-		frame:GetObject().parent:FindDescendantByType("UIContainer").visibility = Visibility.INHERIT
-		frame:GetObject().parent:FindDescendantByName("Picture"):SetBlockchainToken(_G.tokens[address .. tokenId])
-		frame:GetObject().parent:FindDescendantByName("Info").text = _G.tokens[address .. tokenId].name
-	end
-end)
+ConnectSlotEvents()
 
-Game.GetLocalPlayer().privateNetworkedDataChangedEvent:Connect(update_frames)
-update_frames(Game.GetLocalPlayer(), "frames")
+Events.Connect("inventory.open", open_inventory)
+Events.Connect("FetchTokens", function()
+	Task.Wait()
+	canFetchTokens = true
+end)
